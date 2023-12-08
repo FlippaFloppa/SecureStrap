4a8999cc9af739ce890c1dc793e99610892d9263
Lore09 <lollo.venerandi@gmail.com>
2023-12-08 11:25:15 +0100

    Server with button working

--- raspberry-python-client/server.py
+++ raspberry-python-client/server.py
@@ -1,26 +1,81 @@
-import socket
+import bluetooth
 import os
-from dotenv import load_dotenv
+import time
+import Adafruit_SSD1306
+import Adafruit_GPIO
 
-load_dotenv()
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+#----------------------------------------
+#-----VARIABLES--------------------------
+button_wait =2.5
+button_pin = 10
+rasp_mac_address = "DC:A6:32:5A:54:E0"
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+#----------------------------------------
 
-server.bind((os.getenv("BLUETOOTH_SERVER_MAC_ADDR"), 4))
-server.listen(1)
-print("Listening for connections...")
+trigger = False
 
-client, address = server.accept()
-print(f"Connected to {address}")
-try:
-    while True:
-        data = client.recv(1024)
-        if not data:
-            break
-        print(f"Message:  {data.decode()}")
-    
-except OSError:
-    pass
+def button_handler(channel):
+	global start_time, trigger
+	if GPIO.input(button_pin):
+		print("Button pressed")
+		start_time = time.time()
+	else:
+		diff = time.time() - start_time
+		print("Button released after " + str(diff))
+		if diff >= button_wait:
+			trigger = True
 
-client.close()
-server.close()
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(button_pin,GPIO.BOTH,callback=button_handler)
+
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind((rasp_mac_address, bluetooth.PORT_ANY))
+server_sock.listen(1)
+port = server_sock.getsockname()[1]
+
+os.system('sudo bluetoothctl discoverable on')
+bluetooth.advertise_service(
+server_sock, 
+"SampleServer", 
+service_id=uuid,
+service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+profiles=[bluetooth.SERIAL_PORT_PROFILE],
+# protocols=[bluetooth.OBEX_UUID]
+)
+
+while True:
+
+	print("Waiting for connection on RFCOMM channel", port)
+	client_sock, client_info = server_sock.accept()
+	print("Accepted connection from", client_info)
+	try:	
+		while True:
+
+			if trigger:
+
+				message = "notifica#"
+				client_sock.send(message.encode("utf-8"))
+				time.sleep(1/10)
+
+				# message = "sms#"
+				# client_sock.send(message.encode("utf-8"))
+				# time.sleep(1/10)
+				
+				message = "chiama#"
+				client_sock.send(message.encode("utf-8"))
+				time.sleep(1/10)
+
+				trigger = False
+			
+			time.sleep(1/100)
+	except OSError:
+		pass
+	print("Disconnected.")
+	client_sock.close()
+
+server_sock.close()
+print("All done.")
--- raspberry-python-client/test-microphone.py
+++ raspberry-python-client/test-microphone.py
@@ -2,16 +2,22 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 print(sr.Microphone.list_microphone_names())
-dev_index = int(input("Seleziona il microfono: "))
-print("Selected device " + str(dev_index))
-mic = sr.Microphone(device_index = dev_index) 
-
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    print("Listening ...")
-    audio = r.listen(source)
+input("Premi enter")
+mic = sr.Microphone() 
 
 try:
-    print("Messaggio: " + r.recognize_google(audio))
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        while(True):
+            print("Listening ...")
+            audio = r.listen(source = source, timeout = 10, phrase_time_limit = 5)
+
+            #mess = r.recognize_sphinx(audio, 'en-US')
+            mess = r.recognize_sphinx(audio, 'it-IT')
+            print("Messaggio: " + mess)
+
+            if(mess=='basta'):
+                break
+        
 except Exception as e:
     print(e)
