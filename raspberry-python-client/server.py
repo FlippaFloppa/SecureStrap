35db155695064c8134f090f03ad91b2628473c5a
Lore09 <lollo.venerandi@gmail.com>
2023-12-08 12:34:51 +0100

    oled display

--- raspberry-python-client/server.py
+++ raspberry-python-client/server.py
@@ -1,20 +1,48 @@
 import bluetooth
 import os
 import time
-import Adafruit_SSD1306
-import Adafruit_GPIO
-
+import board
+import busio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import datetime
 
 #----------------------------------------
 #-----VARIABLES--------------------------
 button_wait =2.5
-button_pin = 10
+button_pin = 15
 rasp_mac_address = "DC:A6:32:5A:54:E0"
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 #----------------------------------------
 
 trigger = False
+
+def draw_text(text):
+	global draw
+	oled.fill(0)
+
+	draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+
+	draw.rectangle(
+		(BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
+		outline=0,
+		fill=0,
+	)
+
+	font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+	(font_width, font_height) = font.getsize(text)
+	draw.text(
+		(oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
+		text,
+		font=font,
+		fill=255,
+	)
+
+	oled.image(image)
+	oled.show()
+
 
 def button_handler(channel):
 	global start_time, trigger
@@ -28,9 +56,16 @@ def button_handler(channel):
 			trigger = True
 
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+#GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.add_event_detect(button_pin,GPIO.BOTH,callback=button_handler)
+
+i2c = busio.I2C(board.SCL, board.SDA)
+oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+BORDER = 2
+image = Image.new("1", (oled.width, oled.height))
+draw = ImageDraw.Draw(image)
+
 
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 server_sock.bind((rasp_mac_address, bluetooth.PORT_ANY))
@@ -50,16 +85,27 @@ profiles=[bluetooth.SERIAL_PORT_PROFILE],
 while True:
 
 	print("Waiting for connection on RFCOMM channel", port)
+	draw_text("Waiting...")
+
 	client_sock, client_info = server_sock.accept()
 	print("Accepted connection from", client_info)
+
+	draw_text("Connected!")
+	time.sleep(2)
+
 	try:	
-		while True:
+		while client_sock is not None:
+
+			current_time = datetime.datetime.now()
+			draw_text(f'{current_time.hour}:{current_time.minute}:{current_time.second}')
 
 			if trigger:
 
+				draw_text("-- SOS --")
+
 				message = "notifica#"
 				client_sock.send(message.encode("utf-8"))
-				time.sleep(1/10)
+				time.sleep(5/10)
 
 				# message = "sms#"
 				# client_sock.send(message.encode("utf-8"))
@@ -67,15 +113,18 @@ while True:
 				
 				message = "chiama#"
 				client_sock.send(message.encode("utf-8"))
-				time.sleep(1/10)
+				time.sleep(2/10)
 
 				trigger = False
+				time.sleep(3)
 			
-			time.sleep(1/100)
+			time.sleep(1/10)
+	except ConnectionResetError:
+		print("Disconnected.")
+		client_sock.close()
+		break
 	except OSError:
 		pass
-	print("Disconnected.")
-	client_sock.close()
 
 server_sock.close()
 print("All done.")
